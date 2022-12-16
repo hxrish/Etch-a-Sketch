344b0aad1dfb2f2100ecb0191dcabb160db43340
Pureharish <108225503+pureharish@users.noreply.github.com>
2022-12-16 21:23:48 +0530

    Made the grid-size button functioning and added hover color effect

--- test-area/script.js
+++ test-area/script.js
@@ -1,9 +1,49 @@
 let parent = document.getElementById('container')
 
+function clearGrid(){
+    parent.replaceChildren();
+}
 
-function createGrid(){
-let Number = parseInt(prompt()); //get the number of rows
-//  let columnNumber = parseInt(prompt()); //get the number of columns
+
+let fiveByFive = document.getElementById('fiveByFive');
+let tenByTen = document.getElementById('tenByTen');
+let fifteenByFifteen = document.getElementById('fifteenByFifteen');
+let twentyByTwenty = document.getElementById('twentyByTwenty');
+
+fiveByFive.addEventListener('click', clicked_fiveByFive); //Add click event listener to fiveByFive and create function 'clicked_fiveByFive'
+
+tenByTen.addEventListener('click', clicked_tenByTen); //Add click event listener to tenByTen and create function 'clicked_tenByTen'
+
+fifteenByFifteen.addEventListener('click', clicked_fifteenByFifteen); //Add click event listener to fifteenByFifteen and create function 'clicked_fifteenByFifteen'
+
+twentyByTwenty.addEventListener('click', clicked_twentyByTwenty); //Add click event listener to twentyByTwenty and create function 'clicked_twentyByTwenty'
+
+
+function clicked_fiveByFive(){
+    Number = 5;
+    createGrid(Number);
+}
+
+function clicked_tenByTen(){
+    Number = 10;
+    createGrid(Number);
+}
+
+function clicked_fifteenByFifteen(){
+    Number = 15;
+    createGrid(Number);
+}
+
+function clicked_twentyByTwenty(){
+    Number = 20;
+    createGrid(Number);
+}
+
+
+
+function createGrid(Number){
+
+    clearGrid()
 
 for(let i=0; i<Number; i++){
     let row = document.createElement('div');
@@ -20,7 +60,19 @@ for(let i=0; i<Number; i++){
     
     row.appendChild(column);
     }
-} //create rows according to rowNumber
+}
+
+let column = Array.from(document.querySelectorAll('.column'));
+
+console.log(column);
+
+column.forEach(element => {
+    element.addEventListener('mouseover', e => {
+        element.setAttribute("style", "background-color: blue;");  
+    })
+})
+
+//create rows according to rowNumber
 
 // let row = Array.from(document.querySelectorAll('.row'));
 
@@ -37,7 +89,6 @@ for(let i=0; i<Number; i++){
 
 }
 
-createGrid()
 
 
 
